61b07c7d4ee45707f7ae6360c38b4da9815e8ae1
momoko <928701977@qq.com>
2018-05-27 14:49:24 +0800

    fix: vuepress search not work

--- docs/dist/service-worker.js
+++ docs/dist/service-worker.js
@@ -21,27 +21,27 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox
 self.__precacheManifest = [
   {
     "url": "404.html",
-    "revision": "81b8965882ab5679f2815ff964592a44"
+    "revision": "d81256346bb0e14ac0f6ce16e270375e"
   },
   {
-    "url": "assets/css/1.styles.8b1259b1.css",
-    "revision": "00e50c824143587381b49aaba9dadc80"
+    "url": "assets/css/1.styles.cbe77d76.css",
+    "revision": "af2fd06ca730b7d451838b7444dfe3aa"
   },
   {
     "url": "assets/img/search.83621669.svg",
     "revision": "83621669651b9a3d4bf64d1a670ad856"
   },
   {
-    "url": "assets/js/0.ef69fbeb.js",
-    "revision": "b1ecf83fdba1a4feac78f708efd3c6b9"
+    "url": "assets/js/0.7c7da7d7.js",
+    "revision": "afaa99593b35d1102c42fa74d5db2943"
   },
   {
-    "url": "assets/js/app.2ecf2d24.js",
-    "revision": "caa4ab049c93d6e1cdac016fdde0ca5e"
+    "url": "assets/js/app.f003b2b7.js",
+    "revision": "5105fce8e64b69277733f0a5161e92d1"
   },
   {
     "url": "index.html",
-    "revision": "c3c316bb4c7302f1234e0c698842eed7"
+    "revision": "680738409eade1935e63c706bce08e1e"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
