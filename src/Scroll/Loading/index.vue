56664791659a1525684752676ac0ab66be236dca
momoko <ko.momo@qq.com>
2019-04-25 18:11:13 +0800

    refactor: Loading 重构，抛弃 gif，使用 css 实现，体积近一步减小

--- src/Scroll/Loading/index.vue
+++ src/Scroll/Loading/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div :class="c('-loading-container')">
-    <img src="./loading.gif">
+  <div :class="c('__loading')">
+    <i v-for="item in 12" :key="item" />
   </div>
 </template>
 <script type="text/ecmascript-6">
@@ -12,11 +12,51 @@ export default {
 }
 </script>
 <style lang="stylus">
-$ = vue-slim-better-scroll
+$ = vue-slim-better-scroll;
+$color = #8c8c8c;
+.{$}__loading {
+  width: 20px;
+  height: 20px;
+  position: relative;
+  display: block;
+  box-sizing: border-box;
+  animation: spinner-rotate 1s linear infinite;
+  animation-timing-function: steps(12);
+  color: $color;
 
-.{$}-loading-container
-  > img
-    width: 20px
-    height: 20px
-    display: block
+  i {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+
+    &::before {
+      width: 9%;
+      height: 25%;
+      content: '';
+      display: block;
+      margin: 0 auto;
+      border-radius: 10px;
+      background-color: currentColor;
+    }
+  }
+
+  for $i in (1 .. 12) {
+    i:nth-of-type({$i}) {
+      opacity: 1 - (0.75 / 12) * ($i - 1);
+      transform: rotate($i * 30deg);
+    }
+  }
+
+  @keyframes spinner-rotate {
+    from {
+      transform: rotate(0deg);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+}
 </style>
